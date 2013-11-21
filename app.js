b1384ed6a259d45a0c0c395a1b2c77bc83f939dd
Ryan Epp <repp@redtettemer.com>
2013-11-21 00:37:17 -0500

    switched the waiting players from a stack to a queue, cleaned up the index.html and created client.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//Setup
 var app = require('express')()
     , server = require('http').createServer(app)
     , io = require('socket.io').listen(server)
@@ -7,18 +8,23 @@ var app = require('express')()
 server.listen(port);
 
 app.use("/css", express.static(__dirname + '/css'));
-app.use("/vendor_js", express.static(__dirname + '/vendor_js'));
+app.use("/js", express.static(__dirname + '/js'));
+app.use("/js/vendor_js", express.static(__dirname + '/js/vendor_js'));
 app.use("/views", express.static(__dirname + '/views'));
 
+//Routes
 app.get('/', function (req, res) {
     res.sendfile(__dirname + '/views/index.html');
 });
 
+//Connections
 var connections = 0;
 var waitingPlayers = [];
-var games = [];
 
 io.sockets.on('connection', function (socket) {
+
+    socket.currentGame = null;
+
     connections++;
     io.sockets.emit('connection_count', {
         count: connections
@@ -26,23 +32,27 @@ io.sockets.on('connection', function (socket) {
 
     socket.on('disconnect', function () {
         connections--;
+        if(socket.currentGame !== null) {
+            socket.currentGame.destroy();
+        } else {
+            removeFromWaitingPlayers(socket);
+        }
         io.sockets.emit('connection_count', {
             count: connections
         });
     });
 
-    console.log('hit4');
     socket.on('find_game', function(data) {
-        console.log('hit1');
+        console.log("FIND GAME");
+        console.log(waitingPlayers);
         var player = {socket: socket, name: data.name};
         if(waitingPlayers.length > 0) {
-            var newOpponent = waitingPlayers.pop();
+            var newOpponent = waitingPlayers.shift(); // Optimize later
             createGame(player, newOpponent);
-            console.log('CREATE GAME');
+            console.log('got new opponent');
         } else {
             waitingPlayers.push(player);
-            console.log('LIST UP');
-            console.log(waitingPlayers);
+            console.log('added to waiting players');
         }
     });
 
@@ -50,9 +60,16 @@ io.sockets.on('connection', function (socket) {
     function createGame(player1, player2) {
         var game = {
             player1: player1,
-            player2: player2
+            player2: player2,
+            destroy: function() {
+                this.player1.socket.emit('opponent_left');
+                this.player2.socket.emit('opponent_left');
+                this.player1.socket.currentGame = null;
+                this.player2.socket.currentGame = null;
+            }
         };
-        games.push(game);
+        player1.socket.currentGame = game;
+        player2.socket.currentGame = game;
 
         player1.socket.emit('game_found', {
             opponent: {name: player2.name}
@@ -61,7 +78,19 @@ io.sockets.on('connection', function (socket) {
         player2.socket.emit('game_found', {
             opponent: {name: player1.name}
         });
+        return game;
     }
+
+    function removeFromWaitingPlayers(socketToRemove) {
+        for(var i = 0; i < waitingPlayers.length; i++) {
+            if(waitingPlayers[i].socket === socketToRemove) {
+                waitingPlayers.splice(i, 1);
+                console.log('REMOVED: ' + i);
+                console.log(waitingPlayers);
+            }
+        }
+    }
+
 
 });
 
---
+++ js/client.js
@@ -0,0 +1,75 @@
+var socket,
+    playerName,
+    opponentName,
+    $playerName,
+    $opponentName,
+    $connectionCount,
+    $playForm,
+    $findNewOpponent,
+    $action;
+
+$(document).ready(function() {
+    $connectionCount = $('span#connection-count'),
+    $playForm = $('form#login'),
+    $findNewOpponent = $('#find-new-opponent'),
+    $action = $('#network-message');
+
+    $playerName  = $('#player-name');
+    $opponentName = $('#opponent-name');
+
+    $playForm.submit(findGame);
+    $findNewOpponent.click(findGame);
+    openConnection();
+});
+
+function openConnection() {
+    socket = io.connect(window.location.hostname),
+    socket.on('connection_count', function (data) {
+        $connectionCount.html(data.count)
+    });
+}
+
+function findGame(e) {
+    e.preventDefault();
+    var $nameInput = $('#name');
+    playerName = $nameInput.val();
+    if(playerName.length !== 0) {
+        $playerName.html(playerName);
+        showFindOpponentAnimation();
+        findAnOpponent();
+    } else {
+        alert('Please enter your name.');
+        $nameInput.focus();
+    }
+    return false;
+}
+
+function showFindOpponentAnimation() {
+    $playForm.hide();
+    $findNewOpponent.hide();
+    $action.html('Looking for opponent...');
+    $action.show();
+}
+
+function findAnOpponent() {
+    socket.on('game_found', function(data) {
+        opponentName = data.opponent.name;
+        $opponentName.html(opponentName);
+        $action.html('Opponent found. Setting up game...');
+        setupGame();
+    });
+
+    socket.emit('find_game', {
+        name: playerName
+    });
+
+    socket.on('opponent_left', function(data) {
+        $opponentName.html('');
+        $action.html('Your opponent has left the game.');
+        $findNewOpponent.show();
+    });
+}
+
+function setupGame() {
+
+}
